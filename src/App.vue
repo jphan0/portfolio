6e0e0f63d081576ecb55e9995014fa0b49083c8a
James <16962633@student.westernsydney.edu.au>
2019-08-12 22:26:55 +1000

    Added AOC

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,15 @@
 
     <Heading heading="Projects" id="projects"></Heading>
 
+    <ProjectCard title="Alpha Omega Consulting" technologies="HTML5, SCSS, Laravel, Bulma" projectduration="Jun 19" body="A website for an NDIS provider that delivers high quality care services across Sydney to seniors and to those living with disabilities." cardback="blur_back aoc_back" button1="btn btn-light btn-sm shadow-sm bg-white rounded" href="https://aomc.jphan.info" buttontext="Live Demo"></ProjectCard>
+
     <ProjectCard title="Blog" technologies="HTML5, SCSS, Laravel, Bulma" projectduration="Apr 19" body="A small personal blog to document travels, projects, provide guides and tutorials as well as product reviews, and showcase photos." cardback="blur_back blog_back" button1="btn btn-light btn-sm shadow-sm bg-white rounded" href="https://blog.jphan.info" buttontext="Live Demo"></ProjectCard>
 
     <ProjectCard title="Tiddoo" technologies="HTML5, SCSS, Vue, Vuex, Laravel, Pusher, Bootstrap" projectduration="Feb 19" body="From work to play, get stuff done easily using Tiddoo. Tiddoo is a simple to-do web application built with VueJS and Laravel, using Pusher to add realtime functionality." cardback="blur_back tiddoo_back" button1="btn btn-light btn-sm shadow-sm bg-white rounded" href="https://tiddoo.jphan.info" buttontext="Live Demo"></ProjectCard>
 
     <ProjectCard title="Portfolio" technologies="HTML5, CSS3, Vue, Bootstrap" projectduration="Jan 19" body="This page! Have a geez at some of the work I've done. This portfolio was built with VueJS and Bootstrap, an excellent entry point to JavaScript frameworks." cardback="blur_back portfolio_back" button1="btn btn-light btn-sm shadow-sm bg-white rounded" href="https://jphan.info" buttontext="Live Demo"></ProjectCard>
 
-    <ProjectCard title="Minto Family Medical Centre" technologies="HTML5, CSS3, Laravel, Bootstrap" projectduration="Dec 18 - Jan 19" body="A revamped version of a local medical centre's website supporting mobile, tablet, and desktop sizes. Includes updated news section and accounts enabling publication of articles regarding general medical or practice related news." cardback="blur_back mfmc_back" button1="btn btn-light btn-sm shadow-sm bg-white rounded" href="https://mintofamilymedicalcentre.com.au" buttontext="Live Demo"></ProjectCard>
+    <ProjectCard title="Minto Family Medical Centre" technologies="HTML5, CSS3, Laravel, Bootstrap" projectduration="Jan 19" body="A revamped version of a local medical centre's website supporting mobile, tablet, and desktop sizes. Includes updated news section and accounts enabling publication of articles regarding general medical or practice related news." cardback="blur_back mfmc_back" button1="btn btn-light btn-sm shadow-sm bg-white rounded" href="https://mintofamilymedicalcentre.com.au" buttontext="Live Demo"></ProjectCard>
 
     <ProjectCard title="ABC Filters" technologies="HTML5, CSS3, jQuery, Laravel, Bootstrap" projectduration="Jul 18 - Dec 18" body="Applied techniques learnt throughout university - from web development to database design to project management. This web application was designed for a local business to automate their invoicing system and provide an updated responsive site design." cardback="blur_back abcfilters_back" button1="btn btn-light btn-sm shadow-sm bg-white rounded d-none" href="https://abcf.jphan.info" buttontext="Screenshots"></ProjectCard>
 
@@ -108,6 +110,14 @@ export default {
 
 html, body {
   font-family: 'Montserrat', helvetica, arial, sans-serif;
+}
+
+.no-webp .aoc_back {
+  background-image: url("~@/assets/img/aoc.jpg");
+}
+
+.webp .aoc_back{
+  background-image: url("~@/assets/img/aoc.webp");
 }
 
 .no-webp .blog_back {
